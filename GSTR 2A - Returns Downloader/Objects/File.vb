7972f314bf63f3d3a08a310a7ae2acede95fe815
Devil7DK <dineshthangavel47@gmail.com>
2018-08-10 23:02:43 +0530

    Add Class to Hold Info of Downloaded Files

---
+++ GSTR 2A - Returns Downloader/Objects/File.vb
@@ -0,0 +1,36 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Namespace Objects
+    Public Class File
+        Sub New(ByVal Month As String, ByVal src As String, ByVal dest As String)
+            Me.SourceFile = src
+            Me.DestFile = dest
+            Me.Month = Month
+        End Sub
+        Property SourceFile As String
+        Property DestFile As String
+        Property Month As String
+        Sub Move()
+            My.Computer.FileSystem.MoveFile(SourceFile, DestFile, True)
+        End Sub
+    End Class
+End Namespace
